0bbca11cd58a9f943f4689d9a7802922eef4dcd1
Velerium <bondaniandrea@libero.it>
2021-05-30 18:48:11 +0200

    Fixed a critical error I didn't think of earlier. Now month and day of birth are considered as well when calculating age!

--- Biglietto_bonus/js/script.js
+++ Biglietto_bonus/js/script.js
@@ -1,19 +1,53 @@
 var km = prompt('Quanti chilometri hai intenzione di percorrere?');
 km = parseInt(km);
-var year = prompt('Inserisci il tuo anno di nascita');
-year = parseInt(year);
+var ageYear = prompt('Inserisci il tuo anno di nascita');
+ageYear = parseInt(ageYear);
+var month = prompt('Inserisci il tuo mese di nascita (1-12)');
+month = parseInt(month);
+month--;  // Remember: months are from 0 to 11, not 1 to 12!!
+var day = prompt('Inserisci il tuo giorno di nascita (1-31)');
+day = parseInt(day);
 
-if (isNaN(km) || isNaN(year)) {
+if (isNaN(km) || isNaN(ageYear) || isNaN(month) || isNaN(day)) {
     alert('Please insert valid values!')
     location.reload(); // Reloads the page, resetting prompt values.
 }   
-/* Triggers if non-number characters are input in either prompt
+/* Triggers if non-number characters are input in any prompt
    *before* any number.
    If a value includes non-number characters that follow numbers,
    everything after (and including) the first one gets ignored */
 
 var d = new Date;
-var age = d.getFullYear() - year;
+
+var age = d.getFullYear() - ageYear;
+
+if (age === 18) {
+    var ageMonth = d.getMonth();
+
+    if (ageMonth < month) {
+        age = 17;
+
+    }   else if (ageMonth === month) {
+        var ageDay = d.getDate();
+        if (ageDay < day) {
+            age = 17;
+        }
+    }
+}
+
+if (age === 65) {
+    var ageMonth = d.getMonth();
+
+    if (ageMonth > month) {
+        age = 66;
+
+    }   else if (ageMonth === month) {
+        var ageDay = d.getDate();
+        if (ageDay > day) {
+            age = 66;
+        }
+    }
+}
 
 var price = km*0.21;
 
